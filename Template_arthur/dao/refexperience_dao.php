720ba43e39baab23aeb76e74d88700210932c8d0
GabrielGagnier <gagnier.gabriel@hotmail.fr>
2017-03-12 18:34:38 +0100

    create experience

--- Template_arthur/dao/refexperience_dao.php
+++ Template_arthur/dao/refexperience_dao.php
@@ -19,5 +19,25 @@ class RefExperienceDao
         Database::disconnect();
         return self::$data;
     }
+
+    public static function createRefExperience($libelle,$comment)
+    {
+        $log = Log::getLog();
+        $actif = 1;
+        $pdo = Database::connect();
+        $sql = "INSERT INTO refexperience (libelle_refexperience, commentaire_refexperience, 
+            actif_refexperience) 
+            VALUES ( ? , ? , ?)";
+        $req = $pdo->prepare($sql);
+        if($req->execute(array($libelle, $comment, $actif)))
+            $log->info("Création de l'experience ".$libelle.".");
+        else{
+            $log->error("Echec de la création de l'experience ".$libelle.".");
+            Database::disconnect();
+            return 0;
+        }
+        Database::disconnect();
+        return 1;
+    }
 }
 ?>
