7c4c06f600f0989fc21ce1ae0ace68aca31eea7d
GabrielGagnier <gagnier.gabriel@hotmail.fr>
2017-03-12 19:57:54 +0100

    creation de type de donnée

--- Template_arthur/dao/reftypedonnee_dao.php
+++ Template_arthur/dao/reftypedonnee_dao.php
@@ -18,5 +18,23 @@ class RefTypeDonneeDao
         Database::disconnect();
         return self::$data;
     }
+
+    public static function createRefTypeDonnee($libelle,$comment)
+    {
+        $log = Log::getLog();
+        $pdo = Database::connect();
+        $sql = "INSERT INTO reftypedonnee (libelle_reftypedonnee, commentaire_reftypedonnee) 
+            VALUES ( ? , ?)";
+        $req = $pdo->prepare($sql);
+        if($req->execute(array($libelle, $comment)))
+            $log->info("Création du type de donnée ".$libelle.".");
+        else{
+            $log->error("Echec de la création du type de donnée ".$libelle.".");
+            Database::disconnect();
+            return 0;
+        }
+        Database::disconnect();
+        return 1;
+    }
 }
 ?>
