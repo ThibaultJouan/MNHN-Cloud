a48e459bbc0521263c1f67aba9d7f22bfcddeb16
ArthurLorillard <arthur@lorillard.name>
2017-03-16 13:59:42 +0100

    fin de l'ajout du module connection

--- src/view/connection/index.php
+++ src/view/connection/index.php
@@ -19,7 +19,7 @@
 		<![endif]-->
 		
 		<?php 
-			$immage = rand(1, 12);
+			$immage = rand(1, 15);
 		?>
 		<style>
 		body {
@@ -45,15 +45,15 @@
 				<div class="main">
 					<div class="row">
 					<div class="col-xs-12 col-sm-6 col-sm-offset-1">
-						<h1>Ma machine à écrire</h1>
-						<h2>Tous vos textes en un seul endroit</h2>
+						<h1>MNHN Cloud</h1>
+						<h2>Connection</h2>
 						<br>
-						<form action="/users/login" name="login" role="form" class="form-horizontal" method="post" accept-charset="utf-8">
+						<form action="./../../service/connection.php" name="login" role="form" class="form-horizontal" method="post" accept-charset="utf-8">
 							<div class="form-group">
-							<div class="col-md-8"><input name="username" placeholder="Idenfiant" class="form-control" type="text" id="UserUsername"/></div>
+							<div class="col-md-8"><input name="emailConn" placeholder="Mail" class="form-control" type="email" /></div>
 							</div> 
 							<div class="form-group">
-							<div class="col-md-8"><input name="password" placeholder="Mot de passe" class="form-control" type="password" id="UserPassword"/></div>
+							<div class="col-md-8"><input name="mdpConn" placeholder="Mot de passe" class="form-control" type="password"/></div>
 							</div> 
 							<div class="form-group">
 							<div class="col-md-offset-0 col-md-8"><input  class="btn btn-success btn btn-success" type="submit" value="Connexion"/></div>
