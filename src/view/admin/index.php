453ea5328451ffae796f1f6b0aa9301c259dffc1
GabrielGagnier <gagnier.gabriel@hotmail.fr>
2017-04-04 11:38:39 +0200

    correctif session

--- src/view/admin/index.php
+++ src/view/admin/index.php
@@ -4,8 +4,8 @@
     <?php
 		session_start();
 		if($_SESSION ['admin'] != 1){
-			header('Location: ' . '../../index.php');
-			exit();
+		//	header('Location: ' . '../../index.php');
+			exit(var_dump($_SESSION['admin']));
 		}
 		include_once (__DIR__.'/../../dao/utilisateur_dao.php');
 		include_once (__DIR__.'/../../dao/projet_dao.php');
