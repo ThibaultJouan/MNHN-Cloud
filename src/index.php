9dad61a15779045d78e4a4cad9f5eb3503995911
GabrielGagnier <gagnier.gabriel@hotmail.fr>
2017-05-22 11:38:23 +0200

    merge

--- src/index.php
+++ src/index.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
+	    <?php
+			session_start();
+			if(isset($_SESSION ['id_utilisateur'])){
+				header('Location: ' . './view/connection');
+				exit(var_dump($_SESSION['admin']));
+			}
+		?>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
