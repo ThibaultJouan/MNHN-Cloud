16a46fa0888b330eaaf255481c808563c8fa2bc7
thibaultjouan <thibaultjouan@hotmail.com>
2017-05-22 15:35:48 +0200

    modifs sur la creation de dossiers projet

---
+++ src/service/admin/README.md
@@ -0,0 +1,3 @@
+# Creation Projet #
+* ## Point d'attention :
+    * La variable $path dans create_project.php doit etre changee en prod par "/volume1/Projets"
--- src/service/admin/create_project.php
+++ src/service/admin/create_project.php
@@ -10,15 +10,10 @@ if($_POST['libelleProject']) {
     $success = ProjetDao::createProject($libelle,$commentaire);
 		if($success){
 				$src = RefPathDao::getSrcPath();
-				$path = $src['path_refpath'];
+				//$path = $src['path_refpath'];
+				$path = "/var/www/html/MNHN-Cloud/test/";
 				$path = $path."".$libelle;
 				FileTools::makeDirectory($path);
-				$pathA = $path."/A";
-				FileTools::makeDirectory($pathA);
-				$pathB = $path."/B";
-				FileTools::makeDirectory($pathB);
-				$pathC = $path."/C";
-				FileTools::makeDirectory($pathC);
 				header('location:../../view/admin/validate/create_project_validate.html');
 		} else
         header('location:../../view/admin/validate/create_project_denied.html');
