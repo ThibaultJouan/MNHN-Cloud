7bca4b0b3037dab885d1acaff3ca8096d243011f
ArthurLorillard <arthur@lorillard.name>
2017-05-23 11:32:49 +0200

    MAJ SQL

--- SQL/mnhn.sql
+++ SQL/mnhn.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Lun 22 Mai 2017 à 10:34
+-- Généré le :  Mar 23 Mai 2017 à 11:25
 -- Version du serveur :  5.5.47-MariaDB
 -- Version de PHP :  5.5.33
 
@@ -33,8 +33,18 @@ CREATE TABLE IF NOT EXISTS `donnee` (
   `actif_donnee` tinyint(1) NOT NULL,
   `datecreation_donnee` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_utilisateur` int(11) NOT NULL,
-  `id_projet` int(11) NOT NULL,
-  `id_reftypedonnee` int(11) NOT NULL
+  `id_projet` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `experience2utilisateur`
+--
+
+CREATE TABLE IF NOT EXISTS `experience2utilisateur` (
+  `id_utilisateur` int(11) NOT NULL,
+  `id_refexperience` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -49,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `projet` (
   `commentaire_projet` text,
   `actif_projet` tinyint(1) NOT NULL,
   `datecreation_projet` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -69,6 +79,7 @@ CREATE TABLE IF NOT EXISTS `projet2refexperience` (
 --
 
 CREATE TABLE IF NOT EXISTS `projet2utilisateur` (
+  `chef_projet` tinyint(1) DEFAULT NULL,
   `id_utilisateur` int(11) NOT NULL,
   `id_projet` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -90,18 +101,6 @@ CREATE TABLE IF NOT EXISTS `refexperience` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `reftypedonnee`
---
-
-CREATE TABLE IF NOT EXISTS `reftypedonnee` (
-  `id_reftypedonnee` int(11) NOT NULL,
-  `libelle_reftypedonnee` varchar(25) NOT NULL,
-  `commentaire_reftypedonnee` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `utilisateur`
 --
 
@@ -113,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `motdepasse_utilisateur` varchar(25) NOT NULL,
   `actif_utilisateur` tinyint(1) NOT NULL,
   `datecreation_utilisateur` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `admin_utilisateur` tinyint(1) NOT NULL
+  `admin_utilisateur` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -125,9 +124,15 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 --
 ALTER TABLE `donnee`
   ADD PRIMARY KEY (`id_donnee`),
-  ADD KEY `FK_Donnee_id_utilisateur` (`id_utilisateur`),
-  ADD KEY `FK_Donnee_id_projet` (`id_projet`),
-  ADD KEY `FK_Donnee_id_reftypedonnee` (`id_reftypedonnee`);
+  ADD KEY `FK_DONNEE_id_utilisateur` (`id_utilisateur`),
+  ADD KEY `FK_DONNEE_id_projet` (`id_projet`);
+
+--
+-- Index pour la table `experience2utilisateur`
+--
+ALTER TABLE `experience2utilisateur`
+  ADD PRIMARY KEY (`id_utilisateur`,`id_refexperience`),
+  ADD KEY `FK_EXPERIENCE2UTILISATEUR_id_refexperience` (`id_refexperience`);
 
 --
 -- Index pour la table `projet`
@@ -141,14 +146,14 @@ ALTER TABLE `projet`
 --
 ALTER TABLE `projet2refexperience`
   ADD PRIMARY KEY (`id_projet`,`id_refexperience`),
-  ADD KEY `FK_Projet2RefExperience_id_refexperience` (`id_refexperience`);
+  ADD KEY `FK_PROJET2REFEXPERIENCE_id_refexperience` (`id_refexperience`);
 
 --
 -- Index pour la table `projet2utilisateur`
 --
 ALTER TABLE `projet2utilisateur`
   ADD PRIMARY KEY (`id_utilisateur`,`id_projet`),
-  ADD KEY `FK_Projet2Utilisateur_id_projet` (`id_projet`);
+  ADD KEY `FK_PROJET2UTILISATEUR_id_projet` (`id_projet`);
 
 --
 -- Index pour la table `refexperience`
@@ -156,13 +161,6 @@ ALTER TABLE `projet2utilisateur`
 ALTER TABLE `refexperience`
   ADD PRIMARY KEY (`id_refexperience`),
   ADD UNIQUE KEY `libelle_refexperience` (`libelle_refexperience`);
-
---
--- Index pour la table `reftypedonnee`
---
-ALTER TABLE `reftypedonnee`
-  ADD PRIMARY KEY (`id_reftypedonnee`),
-  ADD UNIQUE KEY `libelle_reftypedonnee` (`libelle_reftypedonnee`);
 
 --
 -- Index pour la table `utilisateur`
@@ -190,11 +188,6 @@ ALTER TABLE `projet`
 ALTER TABLE `refexperience`
   MODIFY `id_refexperience` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT pour la table `reftypedonnee`
---
-ALTER TABLE `reftypedonnee`
-  MODIFY `id_reftypedonnee` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
@@ -207,23 +200,29 @@ ALTER TABLE `utilisateur`
 -- Contraintes pour la table `donnee`
 --
 ALTER TABLE `donnee`
-  ADD CONSTRAINT `FK_Donnee_id_projet` FOREIGN KEY (`id_projet`) REFERENCES `projet` (`id_projet`),
-  ADD CONSTRAINT `FK_Donnee_id_reftypedonnee` FOREIGN KEY (`id_reftypedonnee`) REFERENCES `reftypedonnee` (`id_reftypedonnee`),
-  ADD CONSTRAINT `FK_Donnee_id_utilisateur` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
+  ADD CONSTRAINT `FK_DONNEE_id_projet` FOREIGN KEY (`id_projet`) REFERENCES `projet` (`id_projet`),
+  ADD CONSTRAINT `FK_DONNEE_id_utilisateur` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
+
+--
+-- Contraintes pour la table `experience2utilisateur`
+--
+ALTER TABLE `experience2utilisateur`
+  ADD CONSTRAINT `FK_EXPERIENCE2UTILISATEUR_id_refexperience` FOREIGN KEY (`id_refexperience`) REFERENCES `refexperience` (`id_refexperience`),
+  ADD CONSTRAINT `FK_EXPERIENCE2UTILISATEUR_id_utilisateur` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
 
 --
 -- Contraintes pour la table `projet2refexperience`
 --
 ALTER TABLE `projet2refexperience`
-  ADD CONSTRAINT `FK_Projet2RefExperience_id_projet` FOREIGN KEY (`id_projet`) REFERENCES `projet` (`id_projet`),
-  ADD CONSTRAINT `FK_Projet2RefExperience_id_refexperience` FOREIGN KEY (`id_refexperience`) REFERENCES `refexperience` (`id_refexperience`);
+  ADD CONSTRAINT `FK_PROJET2REFEXPERIENCE_id_projet` FOREIGN KEY (`id_projet`) REFERENCES `projet` (`id_projet`),
+  ADD CONSTRAINT `FK_PROJET2REFEXPERIENCE_id_refexperience` FOREIGN KEY (`id_refexperience`) REFERENCES `refexperience` (`id_refexperience`);
 
 --
 -- Contraintes pour la table `projet2utilisateur`
 --
 ALTER TABLE `projet2utilisateur`
-  ADD CONSTRAINT `FK_Projet2Utilisateur_id_projet` FOREIGN KEY (`id_projet`) REFERENCES `projet` (`id_projet`),
-  ADD CONSTRAINT `FK_Projet2Utilisateur_id_utilisateur` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
+  ADD CONSTRAINT `FK_PROJET2UTILISATEUR_id_projet` FOREIGN KEY (`id_projet`) REFERENCES `projet` (`id_projet`),
+  ADD CONSTRAINT `FK_PROJET2UTILISATEUR_id_utilisateur` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
