7a15d62f71430f17f3ba201ee0f01beb33350718
thibaultjouan <thibaultjouan@hotmail.com>
2017-06-07 19:56:10 +0200

    rectification de la navbar

--- src/service/configPath.php
+++ src/service/configPath.php
@@ -1,5 +1,6 @@
 <?php
 
-	define('PATH_PROJET', '/volume1/Projets/');
+	define('PATH_PROJET', '/volume1/Projets/')
 	define('PATH_WEBSITE', '/volume1/web/MNHN-Cloud/');
+	define('PATH_WEB', '/MNHN-Cloud/');
 ?>
