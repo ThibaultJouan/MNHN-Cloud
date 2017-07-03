3ecbaba4d523c963c1fef786d41c2a1eb6cc50e2
ThibaultJouan <thibaultjouan@hotmail.com>
2017-07-03 13:08:05 +0200

    oups

--- src/view/projets/index.php
+++ src/view/projets/index.php
@@ -83,10 +83,13 @@
 					//if($row['id_projet'] == $id_project) {
 						echo '<h1>'. $row['libelle_projet'] .'</h1>';
 						echo '<h3>Experiences : </h3>';
+						if(Projet2UtilisateurDao::isChefProjet($id_project, $id_utilisateur) == 1
+						|| $_SESSION['admin'] == 1){
 							echo '<form action="../admin/update/edit_project2user.php" method="post">';
 							echo '<input type="hidden" name="idProject" value='.$id_project.'>';
 							echo '<input class="btn btn-success btn-sm" type="submit" value="Lier utilisateur">';
 							echo ' </form>';
+						}
 						foreach(RefExperienceDao::selectAllActif() as $exp) {
 							if(1 == Projet2RefExperienceDao::isJoin($id_project, $exp['id_refexperience'])){
 								echo '<tr>';
